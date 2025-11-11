60ea14706e86e3ef244138002f9793977e2be9fd
digitalyoshixi <digitalyoshixi@proton.me>
2025-11-11 15:18:50 -0500

    feat: token enums

---
+++ compiler.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "<vector>"
+#include "<map>"
+
+
+enum Tokens {
+  EOL = -1,
+  Semicolon = 1;
+  LParen = 2,
+  RParen = 3,
+  LCurlyParen = 4,
+  RCurlyParen = 5,
+  LSquareParen = 6,
+  RSquareParen = 7,
+  Star = 8,
+  Slash = 9,
+  Equal = 10,
+  Minus = 11,
+  Plus = 12,
+  Carrot = 13,
+  Identifier = 14,
+  Literal = 15,
+}
+
+int main(){
+  // read input string
+  return 0;
+}
